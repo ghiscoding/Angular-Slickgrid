50832af352578d8fe8a4011e54baaa6454945718
Ghislain Beaulac <gbeaulac@gmail.com>
2017-10-05 00:35:50 -0400

    replace function by lambda

--- src/services/mouse.service.ts
+++ src/services/mouse.service.ts
@@ -1,13 +1,13 @@
 export class MouseService {
   attachOnMouseHover(grid: any) {
-    grid.onMouseEnter.subscribe(function (e: any) {
+    grid.onMouseEnter.subscribe((e: any) => {
       const cell = grid.getCellFromEvent(e);
       if (cell && cell.row >= 0) {
         grid.setSelectedRows([cell.row]);
         e.preventDefault();
       }
     });
-    grid.onMouseLeave.subscribe(function (e: any) {
+    grid.onMouseLeave.subscribe((e: any) => {
       grid.setSelectedRows([]);
       e.preventDefault();
     });
