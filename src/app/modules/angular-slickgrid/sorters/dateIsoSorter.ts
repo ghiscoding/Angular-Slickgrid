91268caa7084bee1e605794cd912acf7ba44c515
Ghislain Beaulac <Ghislain.Beaulac@schneider-electric.com>
2017-10-31 11:40:54 -0400

    use date type to get it's format so that we only change it at 1 place globally

--- src/app/modules/angular-slickgrid/sorters/dateIsoSorter.ts
+++ src/app/modules/angular-slickgrid/sorters/dateIsoSorter.ts
@@ -1,21 +1,15 @@
-import { Sorter } from './../models/index';
-import * as moment_ from 'moment-mini';
-const moment: any = (<any>moment_).default || moment_; // patch to fix rollup "moment has no default export" issue, document here https://github.com/rollup/rollup/issues/670
-const DATE_FORMAT = 'YYYY-MM-DD';
-/*
 import { mapMomentDateFormatWithFieldType } from './../services/utilities';
 import { FieldType, Sorter } from './../models/index';
 import * as moment_ from 'moment-mini';
 const moment: any = (<any>moment_).default || moment_; // patch to fix rollup "moment has no default export" issue, document here https://github.com/rollup/rollup/issues/670
 const FORMAT = mapMomentDateFormatWithFieldType(FieldType.dateIso);
-*/
 
 export const dateIsoSorter: Sorter = (value1, value2, sortDirection) => {
-  if (!moment(value1, DATE_FORMAT, true).isValid() || !moment(value2, DATE_FORMAT, true).isValid()) {
+  if (!moment(value1, FORMAT, true).isValid() || !moment(value2, FORMAT, true).isValid()) {
     return 0;
   }
-  const date1 = moment(value1, DATE_FORMAT, true);
-  const date2 = moment(value2, DATE_FORMAT, true);
+  const date1 = moment(value1, FORMAT, true);
+  const date2 = moment(value2, FORMAT, true);
   const diff = parseInt(date1.format('X'), 10) - parseInt(date2.format('X'), 10);
 
   return sortDirection * (diff === 0 ? 0 : (diff > 0 ? 1 : -1));
--- src/app/modules/angular-slickgrid/sorters/dateUsSorter.ts
+++ src/app/modules/angular-slickgrid/sorters/dateUsSorter.ts
@@ -1,15 +1,15 @@
-import { Sorter } from './../models/index';
+import { mapMomentDateFormatWithFieldType } from './../services/utilities';
+import { FieldType, Sorter } from './../models/index';
 import * as moment_ from 'moment-mini';
 const moment: any = (<any>moment_).default || moment_; // patch to fix rollup "moment has no default export" issue, document here https://github.com/rollup/rollup/issues/670
-
-const DATE_FORMAT = 'M/D/YYYY';
+const FORMAT = mapMomentDateFormatWithFieldType(FieldType.dateUs);
 
 export const dateUsSorter: Sorter = (value1, value2, sortDirection) => {
-  if (!moment(value1, DATE_FORMAT, true).isValid() || !moment(value2, DATE_FORMAT, true).isValid()) {
+  if (!moment(value1, FORMAT, true).isValid() || !moment(value2, FORMAT, true).isValid()) {
     return 0;
   }
-  const date1 = moment(value1, DATE_FORMAT, true);
-  const date2 = moment(value2, DATE_FORMAT, true);
+  const date1 = moment(value1, FORMAT, true);
+  const date2 = moment(value2, FORMAT, true);
   const diff = parseInt(date1.format('X'), 10) - parseInt(date2.format('X'), 10);
 
   return sortDirection * (diff === 0 ? 0 : (diff > 0 ? 1 : -1));
