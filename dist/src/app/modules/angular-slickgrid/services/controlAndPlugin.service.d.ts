c72cdb5d8186e72c1409ac68d88c83362ac9ccbf
Ghislain Beaulac <Ghislain.Beaulac@schneider-electric.com>
2017-11-08 15:40:28 -0500

    prepare release 0.5.4

--- dist/src/app/modules/angular-slickgrid/services/controlAndPlugin.service.d.ts
+++ dist/src/app/modules/angular-slickgrid/services/controlAndPlugin.service.d.ts
@@ -1,9 +1,23 @@
+import { Router } from '@angular/router';
+import { FilterService } from './filter.service';
 import { Column, GridOption } from './../models';
 export declare class ControlAndPluginService {
+    private filterService;
+    private router;
+    _dataView: any;
     _grid: any;
     _visibleColumns: Column[];
+    autoTooltipPlugin: any;
+    columnPickerControl: any;
+    headerButtonsPlugin: any;
+    headerMenuPlugin: any;
+    gridMenuControl: any;
+    rowSelectionPlugin: any;
+    constructor(filterService: FilterService, router: Router);
     attachDifferentControlOrPlugins(grid: any, columnDefinitions: Column[], options: GridOption, dataView: any): void;
     hideColumn(column: Column): void;
     removeColumnByIndex(array: any, index: any): any;
     autoResizeColumns(): void;
+    private addGridMenuCustomCommands(options);
+    private prepareGridMenu(options);
 }
