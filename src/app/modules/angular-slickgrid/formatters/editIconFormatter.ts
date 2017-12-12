200962f80d7eb823b7d052cc1585b30c1f8c897e
Ghislain Beaulac <Ghislain.Beaulac@schneider-electric.com>
2017-12-12 13:44:50 -0500

    add extra classes for styling purposes

--- src/app/modules/angular-slickgrid/formatters/editIconFormatter.ts
+++ src/app/modules/angular-slickgrid/formatters/editIconFormatter.ts
@@ -1,4 +1,4 @@
 import { Formatter } from './../models/formatter.interface';
 
 export const editIconFormatter: Formatter = (row: number, cell: number, value: any, columnDef: any, dataContext: any) =>
-  `<i class="fa fa-pencil pointer" aria-hidden="true"></i>`;
+  `<i class="fa fa-pencil pointer edit-icon" aria-hidden="true"></i>`;
