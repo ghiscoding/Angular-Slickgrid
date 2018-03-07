7052781f5a78c252b5b87b25f7b147b58346c369
Ghislain Beaulac <Ghislain.Beaulac@schneider-electric.com>
2018-03-07 16:20:17 -0500

    fix(formatter): multipleFormatter should pass value to next formatter - the formatter should be execute in a pipe mode (1st formatter value will be passed to 2nd formatter, ...)

--- src/app/modules/angular-slickgrid/formatters/multipleFormatter.ts
+++ src/app/modules/angular-slickgrid/formatters/multipleFormatter.ts
@@ -1,15 +1,18 @@
 import { Column, Formatter } from './../models/index';
 
-export const multipleFormatter: Formatter = (row: number, cell: number, value: any, columnDef: Column, dataContext: any) => {
+export const multipleFormatter: Formatter = (row: number, cell: number, value: any, columnDef: Column, dataContext: any, grid: any) => {
   const params = columnDef.params || {};
   if (!params.formatters || !Array.isArray(params.formatters)) {
     throw new Error(`The multiple formatter requires the "formatters" to be provided as a column params.
     For example: this.columnDefinitions = [{ id: title, field: title, formatter: Formatters.multiple, params: { formatters: [Formatters.lowercase, Formatters.uppercase] }`);
   }
   const formatters: Formatter[] = params.formatters;
-  let formattedValue = '';
+
+  // loop through all Formatters, the value of 1st formatter will be used by 2nd formatter and so on.
+  // they are piped and executed in sequences
+  let currentValue = value;
   for (const formatter of formatters) {
-    formattedValue = formatter(row, cell, value, columnDef, dataContext);
+    currentValue = formatter(row, cell, currentValue, columnDef, dataContext, grid);
   }
-  return formattedValue;
+  return currentValue;
 };
