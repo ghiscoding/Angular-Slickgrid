fd94cd4a91c685b40694599c3cac4bf323367740
Ghislain Beaulac <Ghislain.Beaulac@schneider-electric.com>
2018-05-22 18:17:49 -0400

    refactor(grid): re-expose all the Angular-Slickgrid services - not sure how to do around this yet, but without exposing all the Services, a user cannot create it's own custom BackendServiceApi (example for Oracle)

--- src/app/modules/angular-slickgrid/modules/angular-slickgrid.module.ts
+++ src/app/modules/angular-slickgrid/modules/angular-slickgrid.module.ts
@@ -1,12 +1,23 @@
+import { SharedService } from './../services/shared.service';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { AngularSlickgridComponent } from './../components/angular-slickgrid.component';
 import { CollectionService } from './../services/collection.service';
+import { ControlAndPluginService } from '../services/controlAndPlugin.service';
+import { ExportService } from '../services/export.service';
+import { FilterService } from './../services/filter.service';
 import { GraphqlService } from './../services/graphql.service';
 import { GridOdataService } from './../services/grid-odata.service';
 import { GridOption } from './../models/gridOption.interface';
+import { GridEventService } from './../services/gridEvent.service';
+import { GridService } from './../services/grid.service';
+import { GridStateService } from './../services/gridState.service';
+import { GroupingAndColspanService } from './../services/groupingAndColspan.service';
+import { OdataService } from './../services/odata.service';
+import { ResizerService } from './../services/resizer.service';
 import { SlickPaginationComponent } from './../components/slick-pagination.component';
+import { SortService } from './../services/sort.service';
 
 @NgModule({
   imports: [
@@ -30,8 +41,19 @@ export class AngularSlickgridModule {
       providers: [
         { provide: 'config', useValue: config },
         CollectionService,
+        ControlAndPluginService,
+        ExportService,
+        FilterService,
         GraphqlService,
+        GridEventService,
+        GridService,
         GridOdataService,
+        GridStateService,
+        GroupingAndColspanService,
+        OdataService,
+        ResizerService,
+        SharedService,
+        SortService
       ]
     };
   }
