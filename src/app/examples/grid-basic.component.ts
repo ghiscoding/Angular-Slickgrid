c691bc3e1047bbd47702f69e3a75007ea60e77a3
Ghislain Beaulac <Ghislain.Beaulac@schneider-electric.com>
2018-05-28 12:29:04 -0400

    refactor(grid): add 2x grids in the Basic Grid sample

--- src/app/examples/grid-basic.component.ts
+++ src/app/examples/grid-basic.component.ts
@@ -13,12 +13,15 @@ export class GridBasicComponent implements OnInit {
     </ul>
   `;
 
-  columnDefinitions: Column[];
-  gridOptions: GridOption;
-  dataset: any[];
+  columnDefinitions1: Column[];
+  columnDefinitions2: Column[];
+  gridOptions1: GridOption;
+  gridOptions2: GridOption;
+  dataset1: any[];
+  dataset2: any[];
 
   ngOnInit(): void {
-    this.columnDefinitions = [
+    this.columnDefinitions1 = [
       { id: 'title', name: 'Title', field: 'title', sortable: true },
       { id: 'duration', name: 'Duration (days)', field: 'duration', sortable: true },
       { id: '%', name: '% Complete', field: 'percentComplete', sortable: true },
@@ -26,19 +29,30 @@ export class GridBasicComponent implements OnInit {
       { id: 'finish', name: 'Finish', field: 'finish' },
       { id: 'effort-driven', name: 'Effort Driven', field: 'effortDriven', sortable: true }
     ];
-    this.gridOptions = {
-      enableAutoResize: false
+    this.gridOptions1 = {
+      enableAutoResize: false,
+      enableSorting: true
     };
 
+    // copy the same Grid Options and Column Definitions to 2nd grid
+    this.columnDefinitions2 = this.columnDefinitions1;
+    this.gridOptions2 = this.gridOptions1;
+
+    // mock some data (different in each dataset)
+    this.dataset1 = this.mockData();
+    this.dataset2 = this.mockData();
+  }
+
+  mockData() {
     // mock a dataset
-    this.dataset = [];
+    const mockDataset = [];
     for (let i = 0; i < 1000; i++) {
       const randomYear = 2000 + Math.floor(Math.random() * 10);
       const randomMonth = Math.floor(Math.random() * 11);
       const randomDay = Math.floor((Math.random() * 29));
       const randomPercent = Math.round(Math.random() * 100);
 
-      this.dataset[i] = {
+      mockDataset[i] = {
         id: i,
         title: 'Task ' + i,
         duration: Math.round(Math.random() * 100) + '',
@@ -48,5 +62,7 @@ export class GridBasicComponent implements OnInit {
         effortDriven: (i % 5 === 0)
       };
     }
+
+    return mockDataset;
   }
 }
