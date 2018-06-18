15e513bc499ba511541329cbf2ecf5cd536de24b
Ghislain Beaulac <Ghislain.Beaulac@schneider-electric.com>
2018-06-18 10:00:08 -0400

    (refactor): export filter factory

--- src/app/modules/angular-slickgrid/index.ts
+++ src/app/modules/angular-slickgrid/index.ts
@@ -6,11 +6,13 @@ export * from './aggregators/index';
 export * from './editors/index';
 export * from './filter-conditions/index';
 export * from './filters/index';
+export * from './filters/filterFactory';
 export * from './formatters/index';
 export * from './grouping-formatters/index';
 export * from './sorters/index';
 
 // components & module
+export { SlickgridConfig } from './slickgrid-config';
 export { SlickPaginationComponent } from './components/slick-pagination.component';
 export { AngularSlickgridComponent } from './components/angular-slickgrid.component';
 export { AngularSlickgridModule } from './modules/angular-slickgrid.module';
