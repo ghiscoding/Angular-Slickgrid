423a3ed2879fa364490f81bf47858f493b384f3d
Ghislain Beaulac <Ghislain.Beaulac@schneider-electric.com>
2018-07-09 17:23:36 -0400

    fix(formatter): don't use strict date check since UTC on Date Formatters - apparently C# date UTC date format is invalid in strict mode because it has millisecond inside it (i.e.: "2017-09-22T12:47:11.247Z")

--- src/app/modules/angular-slickgrid/formatters/dateTimeIsoAmPmFormatter.ts
+++ src/app/modules/angular-slickgrid/formatters/dateTimeIsoAmPmFormatter.ts
@@ -5,6 +5,6 @@ const moment = moment_; // patch to fix rollup "moment has no default export" is
 const FORMAT = mapMomentDateFormatWithFieldType(FieldType.dateTimeIsoAmPm);
 
 export const dateTimeIsoAmPmFormatter: Formatter = (row: number, cell: number, value: any, columnDef: Column, dataContext: any) => {
-  const isDateValid = moment(value, FORMAT, true).isValid();
+  const isDateValid = moment(value, FORMAT, false).isValid();
   return (value && isDateValid) ? moment(value).format(FORMAT) : value;
 };
