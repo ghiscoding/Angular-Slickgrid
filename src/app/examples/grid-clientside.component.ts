72ed904ec1a3ff64b0875cadc90f15f18d6efca0
Ghislain Beaulac <SESA213338@se.com>
2018-10-04 17:13:09 -0400

    feat(select):  add option to use label as selected text - in some cases the selected text (option text) might be too wide to show as selected text, this PR adds possibility to use a different property with shorter text

--- src/app/examples/grid-clientside.component.ts
+++ src/app/examples/grid-clientside.component.ts
@@ -78,6 +78,7 @@ export class GridClientSideComponent implements OnInit {
           customStructure: {
             value: 'value',
             label: 'label',
+            optionLabel: 'value', // if selected text is too long, we can use option labels instead
             labelSuffix: 'text',
           },
           collectionOptions: {
@@ -89,7 +90,11 @@ export class GridClientSideComponent implements OnInit {
           // we could add certain option(s) to the "multiple-select" plugin
           filterOptions: {
             maxHeight: 250,
-            width: 175
+            width: 175,
+
+            // if we want to display shorter text as the selected text (on the select filter itself, parent element)
+            // we can use "useSelectOptionLabel" or "useSelectOptionLabelToHtml" the latter will parse html
+            useSelectOptionLabelToHtml: true
           }
         }
       },
