652a361713e4c95b85ee3a8e8f1de21a9750264d
Edward Boggis-Rolfe <edwardbr@users.noreply.github.com>
2019-01-11 14:54:27 +0000

    Now uses a more restrictive set of return values.

--- src/app/modules/angular-slickgrid/models/formatter.interface.ts
+++ src/app/modules/angular-slickgrid/models/formatter.interface.ts
@@ -1,3 +1,2 @@
-import { Column } from './column.interface';
-
-export type Formatter = (row: number, cell: number, value: any, columnDef: Column, dataContext: any, grid?: any) => any;
+import {Column} from './column.interface';
+export declare type Formatter = (row: number, cell: number, value: any, columnDef: Column, dataContext: any, grid?: any) => string | {text, removeClasses, addClasses};
