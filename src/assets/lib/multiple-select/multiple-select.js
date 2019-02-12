23a6547d9a458baaf75137df057f292a298c93cb
Ghislain Beaulac <SESA213338@se.com>
2019-02-12 17:24:19 -0500

    fix(select): again related to commit cdcc979 - when the click event is not unbound it will not trigger an Angular event, and doing a `$(document).off('click')` was working but it broke Bootstrap click events, however if we use "body" instead of "document", it works and doesn't break Bootstrap click events.

--- src/assets/lib/multiple-select/multiple-select.js
+++ src/assets/lib/multiple-select/multiple-select.js
@@ -209,7 +209,7 @@
     this.selectItemName = 'data-name="selectItem' + name + '"';
 
     if (!this.options.keepOpen) {
-      $(document).off('click.multiple-select').on('click.multiple-select', function (e) {
+      $('body').off('click').on('click', function (e) {
         if ($(e.target)[0] === that.$choice[0] ||
           $(e.target).parents('.ms-choice')[0] === that.$choice[0]) {
           return;
