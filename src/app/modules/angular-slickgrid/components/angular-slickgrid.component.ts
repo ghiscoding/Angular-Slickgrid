10880e750e858d1c34a369e9fbb7bf957c919832
Ghislain Beaulac <SESA213338@se.com>
2019-02-21 10:04:34 -0500

    fix(rowDetail): don't add extra onRowsChanged subscribe with this plugin - RowDetail already has a subscribe with "onRowsChanged" with an "invalidateRows", so if we add an "updateRow" on top of it, it makes the Row Detail behave incorrectly and it's unnecessary anyway.

--- src/app/modules/angular-slickgrid/components/angular-slickgrid.component.ts
+++ src/app/modules/angular-slickgrid/components/angular-slickgrid.component.ts
@@ -478,7 +478,7 @@ export class AngularSlickgridComponent implements AfterViewInit, OnDestroy, OnIn
       // without this, filtering data with local dataset will not always show correctly
       // also don't use "invalidateRows" since it destroys the entire row and as bad user experience when updating a row
       // see commit: https://github.com/ghiscoding/Angular-Slickgrid/commit/bb62c0aa2314a5d61188ff005ccb564577f08805
-      if (gridOptions && gridOptions.enableFiltering) {
+      if (gridOptions && gridOptions.enableFiltering && !gridOptions.enableRowDetailView) {
         this._eventHandler.subscribe(dataView.onRowsChanged, (e: any, args: any) => {
           if (args && args.rows && Array.isArray(args.rows)) {
             args.rows.forEach((row) => grid.updateRow(row));
