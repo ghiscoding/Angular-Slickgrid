cf48af033a6baf8c14e2dcc4956227628a88882f
Ghislain Beaulac <SESA213338@se.com>
2019-03-22 09:40:38 -0400

    refactor(e2e): add Example 6 Cypress tests

--- cypress/integration/example1.spec.js
+++ cypress/integration/example1.spec.js
@@ -1,11 +1,9 @@
 /// <reference types="Cypress" />
 
-describe('Example - Basic Grid', () => {
+describe('Example 1 - Basic Grids', () => {
   it('should display Basic Grid page', () => {
-    cy.visit('http://localhost:4300');
-    cy.get('h2').should(($h2) => {
-      expect($h2, 'text content').to.have.text('Example 1: Basic Grid');
-    });
+    cy.visit(`${Cypress.config('baseExampleUrl')}`);
+    cy.get('h2').should('contain', 'Example 1: Basic Grid');
   });
 
   it('should have 2 grids of size 800 by 300px', () => {
