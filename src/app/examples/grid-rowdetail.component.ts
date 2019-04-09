88c810b2e8d8af4a8e63bf3ae47b47460ec39feb
Ghislain Beaulac <SESA213338@se.com>
2019-04-09 14:47:07 -0400

    refactor(rowDetail): comment out expandableOverride from example

--- src/app/examples/grid-rowdetail.component.ts
+++ src/app/examples/grid-rowdetail.component.ts
@@ -103,7 +103,7 @@ export class GridRowDetailComponent implements OnInit {
 
         // you can override the logic for showing (or not) the expand icon
         // for example, display the expand icon only on every 2nd row
-        expandableOverride: (row: number, dataContext: any, grid: any) => (dataContext.id % 2 === 1),
+        // expandableOverride: (row: number, dataContext: any, grid: any) => (dataContext.id % 2 === 1),
 
         // Preload View Template
         preloadComponent: RowDetailPreloadComponent,
