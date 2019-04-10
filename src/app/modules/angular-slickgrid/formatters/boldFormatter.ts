8a1a3f1842c62f13570d82d8a2fc47f664ca1e8c
Ghislain Beaulac <SESA213338@se.com>
2019-04-10 10:44:54 -0400

    fix(formatter): rewrite formatter do what it was supposed to do - while writting a unit test, we found out the Bold Formatter was dealing with decimal values while it shouldn't

--- src/app/modules/angular-slickgrid/formatters/boldFormatter.ts
+++ src/app/modules/angular-slickgrid/formatters/boldFormatter.ts
@@ -1,13 +1,5 @@
 import { Column, Formatter } from './../models/index';
-import { decimalFormatted } from './../services/utilities';
 
 export const boldFormatter: Formatter = (row: number, cell: number, value: any, columnDef: Column, dataContext: any) => {
-  const isNumber = (value === null || value === undefined || value === '') ? false : !isNaN(+value);
-  if (!isNumber) {
-    return '';
-  } else if (value >= 0) {
-    return `<span style="font-weight: bold">${decimalFormatted(value, 2, 2)}$</span>`;
-  } else {
-    return `<span style="font-weight: bold">${decimalFormatted(value, 2, 2)}$</span>`;
-  }
+  return value ? `<span style="font-weight: bold">${value}</span>` : '';
 };
