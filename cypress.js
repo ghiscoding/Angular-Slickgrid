028c4414356efbfa599bdc691b6196350ca0cabb
Ghislain Beaulac <SESA213338@se.com>
2019-04-24 10:09:18 -0400

    feat(build): add Cypress CI to build plan - 2 report output with mochawesome and xunit to xml

---
+++ cypress.js
@@ -0,0 +1,14 @@
+const cypress = require('cypress')
+const fse = require('fs-extra')
+const { merge } = require('mochawesome-merge')
+const generator = require('mochawesome-report-generator')
+
+async function runTests() {
+  await fse.remove('mochawesome-report') // remove the report folder
+  const { totalFailed } = await cypress.run() // get the number of failed tests
+  const jsonReport = await merge() // generate JSON report
+  await generator.create(jsonReport)
+  process.exit(totalFailed) // exit with the number of failed tests
+}
+
+runTests()
