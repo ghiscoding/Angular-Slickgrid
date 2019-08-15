a9c7e823f0f6058d284c949b97f64de6ce83862f
Ghislain Beaulac <SESA213338@se.com>
2019-08-15 19:43:42 -0400

    refactor(formatter): small update to a formatter's comment

--- src/app/modules/angular-slickgrid/formatters/index.ts
+++ src/app/modules/angular-slickgrid/formatters/index.ts
@@ -143,8 +143,12 @@ export const Formatters = {
   /**
    * Takes an hyperlink cell value and transforms it into a real hyperlink, given that the value starts with 1 of these (http|ftp|https).
    * The structure will be "<a href="hyperlink">hyperlink</a>"
+   *
    * You can optionally change the hyperlink text displayed by using the generic params "hyperlinkText" in the column definition
    * For example: { id: 'link', field: 'link', params: { hyperlinkText: 'Company Website' } } will display "<a href="link">Company Website</a>"
+   *
+   * You can also optionally provide the hyperlink URL by using the generic params "hyperlinkUrl" in the column definition
+   * For example: { id: 'link', field: 'link', params: {  hyperlinkText: 'Company Website', hyperlinkUrl: 'http://www.somewhere.com' } } will display "<a href="http://www.somewhere.com">Company Website</a>"
    */
   hyperlink: hyperlinkFormatter,
 
