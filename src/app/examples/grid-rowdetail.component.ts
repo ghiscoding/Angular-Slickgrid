8257e6986cc48138776fa2bdad2f3830a6bbccbc
Ghislain Beaulac <SESA213338@se.com>
2019-08-23 19:36:04 -0400

    refactor(example): add Row Detail dynamic method call demo

--- src/app/examples/grid-rowdetail.component.ts
+++ src/app/examples/grid-rowdetail.component.ts
@@ -20,7 +20,7 @@ const NB_ITEMS = 500;
 export class GridRowDetailComponent implements OnInit {
   title = 'Example 21: Row Detail View';
   subTitle = `
-    Add functionality to show extra information with a Row Detail View
+    Add functionality to show extra information with a Row Detail View, (<a href="https://github.com/ghiscoding/Angular-Slickgrid/wiki/Row-Detail" target="_blank">Wiki docs</a>)
     <ul>
       <li>Click on the row "+" icon or anywhere on the row to open it (the latter can be changed via property "useRowClick: false")</li>
       <li>Pass a View/Model as a Template to the Row Detail</li>
@@ -41,6 +41,10 @@ export class GridRowDetailComponent implements OnInit {
 
   angularGridReady(angularGrid: AngularGridInstance) {
     this.angularGrid = angularGrid;
+  }
+
+  get rowDetailInstance(): any {
+    return this.angularGrid && this.angularGrid.extensionService.getSlickgridAddonInstance(ExtensionName.rowDetailView) || {};
   }
 
   ngOnInit(): void {
@@ -86,7 +90,7 @@ export class GridRowDetailComponent implements OnInit {
       rowDetailView: {
         // We can load the "process" asynchronously in 2 different ways (httpClient OR even Promise)
         process: (item) => this.simulateServerAsyncCall(item),
-        // process: this.httpFetch.fetch(`api/item/${item.id}`),
+        // process: (item) => this.http.get(`api/item/${item.id}`),
 
         // load only once and reuse the same item detail without calling process method
         loadOnce: true,
@@ -108,10 +112,10 @@ export class GridRowDetailComponent implements OnInit {
         // for example, display the expand icon only on every 2nd row
         // expandableOverride: (row: number, dataContext: any, grid: any) => (dataContext.id % 2 === 1),
 
-        // Preload View Template
+        // Preload View Component
         preloadComponent: RowDetailPreloadComponent,
 
-        // ViewModel Template to load when row detail data is ready
+        // View Component to load when row detail data is ready
         viewComponent: RowDetailViewComponent,
       }
     };
@@ -144,10 +148,17 @@ export class GridRowDetailComponent implements OnInit {
 
   changeDetailViewRowCount() {
     if (this.angularGrid && this.angularGrid.extensionService) {
-      const rowDetailInstance = this.angularGrid.extensionService.getSlickgridAddonInstance(ExtensionName.rowDetailView);
-      const options = rowDetailInstance.getOptions();
-      options.panelRows = this.detailViewRowCount; // change number of rows dynamically
-      rowDetailInstance.setOptions(options);
+      const options = this.rowDetailInstance.getOptions();
+      if (options && options.panelRows) {
+        options.panelRows = this.detailViewRowCount; // change number of rows dynamically
+        this.rowDetailInstance.setOptions(options);
+      }
+    }
+  }
+
+  closeAllRowDetail() {
+    if (this.angularGrid && this.angularGrid.extensionService) {
+      this.rowDetailInstance.collapseAll();
     }
   }
 
