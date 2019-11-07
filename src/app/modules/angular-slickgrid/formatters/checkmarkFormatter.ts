8e7e480dfc21930d3212b8e839f39fa1d7e278b7
Ghislain Beaulac <ghislain.beaulac@se.com>
2019-11-07 18:18:16 -0500

    refactor(code): fix TSlint by adding missing semicolon

--- src/app/modules/angular-slickgrid/formatters/checkmarkFormatter.ts
+++ src/app/modules/angular-slickgrid/formatters/checkmarkFormatter.ts
@@ -4,4 +4,4 @@ import { parseBoolean } from '../services/utilities';
 
 export const checkmarkFormatter: Formatter = (row: number, cell: number, value: any, columnDef: Column, dataContext: any) => {
   return parseBoolean(value) ? `<i class="fa fa-check checkmark-icon" aria-hidden="true"></i>` : '';
-}
+};
