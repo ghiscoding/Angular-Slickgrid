3cc7b20bcbc0215f5e5a2914234cd755dabbb7ec
Ghislain Beaulac <ghislain.beaulac@se.com>
2020-06-26 08:04:38 -0400

    refactor(tests): remove unnecessary moment-timezone

--- test/setup-jest.ts
+++ test/setup-jest.ts
@@ -1,7 +1,2 @@
 import 'jest-preset-angular';
 import './jest-global-mocks';
-
-// use NewYork timezone as default accross the Jest tests
-const moment = require('moment-timezone');
-moment.tz.setDefault('America/New_York');
-jest.setMock('moment', moment)
