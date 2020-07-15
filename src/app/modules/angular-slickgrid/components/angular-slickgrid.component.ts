2d96a9e56e7b9d205d5af58132c1c4625ff6c6bd
Ghislain Beaulac <ghislain.beaulac@se.com>
2020-07-15 10:46:53 -0400

    refactor(core): fix failing unit test

--- src/app/modules/angular-slickgrid/components/angular-slickgrid.component.ts
+++ src/app/modules/angular-slickgrid/components/angular-slickgrid.component.ts
@@ -991,7 +991,7 @@ export class AngularSlickgridComponent implements AfterViewInit, OnDestroy, OnIn
     // using jQuery extend to do a deep clone has an unwanted side on objects and pageSizes but ES6 spread has other worst side effects
     // so we will just overwrite the pageSizes when needed, this is the only one causing issues so far.
     // jQuery wrote this on their docs:: On a deep extend, Object and Array are extended, but object wrappers on primitive types such as String, Boolean, and Number are not.
-    if (gridOptions.enablePagination && (gridOptions.enablePagination || gridOptions.backendServiceApi) && Array.isArray(gridOptions.pagination.pageSizes)) {
+    if (options && options.pagination && (gridOptions.enablePagination || gridOptions.backendServiceApi) && Array.isArray(gridOptions.pagination.pageSizes)) {
       options.pagination.pageSizes = gridOptions.pagination.pageSizes;
     }
 
