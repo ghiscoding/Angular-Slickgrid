fcff2e63b8bd0817abb1bc2a48f63ff1ec14abba
Ghislain Beaulac <ghislain.beaulac@se.com>
2020-07-20 17:10:02 -0400

    fix(menu): context menu to copy cell with queryFieldNameGetterFn

--- src/app/modules/angular-slickgrid/extensions/contextMenuExtension.ts
+++ src/app/modules/angular-slickgrid/extensions/contextMenuExtension.ts
@@ -20,7 +20,7 @@ import { ExportService } from '../services/export.service';
 import { ExcelExportService } from '../services/excelExport.service';
 import { TreeDataService } from '../services/treeData.service';
 import { exportWithFormatterWhenDefined } from '../services/export-utilities';
-import { getTranslationPrefix } from '../services/utilities';
+import { getDescendantProperty, getTranslationPrefix } from '../services/utilities';
 
 // using external non-typed js libraries
 declare const Slick: any;
@@ -194,7 +194,12 @@ export class ContextMenuExtension implements Extension {
               // make sure there's an item to copy before enabling this command
               const columnDef = args && args.column as Column;
               const dataContext = args && args.dataContext;
-              if (columnDef && dataContext.hasOwnProperty(columnDef.field)) {
+              if (typeof columnDef.queryFieldNameGetterFn === 'function') {
+                const cellValue = this.getCellValueFromQueryFieldGetter(columnDef, dataContext);
+                if (cellValue !== '' && cellValue !== undefined) {
+                  return true;
+                }
+              } else if (columnDef && dataContext.hasOwnProperty(columnDef.field)) {
                 return dataContext[columnDef.field] !== '' && dataContext[columnDef.field] !== null && dataContext[columnDef.field] !== undefined;
               }
               return false;
@@ -376,11 +381,15 @@ export class ContextMenuExtension implements Extension {
         const gridOptions = this.sharedService && this.sharedService.gridOptions || {};
         const cell = args && args.cell || 0;
         const row = args && args.row || 0;
-        const column = args && args.column;
+        const columnDef = args && args.column;
         const dataContext = args && args.dataContext;
         const grid = this.sharedService && this.sharedService.grid;
         const exportOptions = gridOptions && (gridOptions.excelExportOptions || gridOptions.exportOptions);
-        const textToCopy = exportWithFormatterWhenDefined(row, cell, dataContext, column, grid, exportOptions);
+        let textToCopy = exportWithFormatterWhenDefined(row, cell, dataContext, columnDef, grid, exportOptions);
+
+        if (typeof columnDef.queryFieldNameGetterFn === 'function') {
+          textToCopy = this.getCellValueFromQueryFieldGetter(columnDef, dataContext);
+        }
 
         // create fake <div> to copy into clipboard & delete it from the DOM once we're done
         const range = document.createRange();
@@ -398,5 +407,28 @@ export class ContextMenuExtension implements Extension {
         }
       }
     } catch (e) { }
+  }
+
+  /**
+   * When a queryFieldNameGetterFn is defined, then get the value from that getter callback function
+   * @param columnDef
+   * @param dataContext
+   * @return cellValue
+   */
+  private getCellValueFromQueryFieldGetter(columnDef: Column, dataContext: any): string {
+    let cellValue = '';
+
+    if (typeof columnDef.queryFieldNameGetterFn === 'function') {
+      const queryFieldName = columnDef.queryFieldNameGetterFn(dataContext);
+
+      // get the cell value from the item or when it's a dot notation then exploded the item and get the final value
+      if (queryFieldName && queryFieldName.indexOf('.') >= 0) {
+        cellValue = getDescendantProperty(dataContext, queryFieldName);
+      } else {
+        cellValue = dataContext[queryFieldName];
+      }
+    }
+
+    return cellValue;
   }
 }
