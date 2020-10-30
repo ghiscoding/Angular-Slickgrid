1c9c7bf78f7d0695ec1a80fdb4cb1a5e210a3f07
Ghislain Beaulac <SESA213338@se.com>
2020-10-30 10:09:23 -0400

    fix: use correct cypress base example url

--- test/cypress/integration/example01.spec.js
+++ test/cypress/integration/example01.spec.js
@@ -4,7 +4,7 @@ describe('Example 1 - Basic Grids', () => {
   const fullTitles = ['Title', 'Duration (days)', '% Complete', 'Start', 'Finish', 'Effort Driven'];
 
   it('should display Example title', () => {
-    cy.visit(`${Cypress.config('baseUrl')}`);
+    cy.visit(`${Cypress.config('baseExampleUrl')}`);
     cy.get('h2').should('contain', 'Example 1: Basic Grids');
   });
 
