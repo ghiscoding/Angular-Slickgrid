638422bf684e620ca9e9f7be138e10cf5e8d62dc
ghiscoding <gbeaulac@gmail.com>
2020-12-02 09:18:26 -0500

    refactor: remove code that was meant for another PR

--- src/app/modules/angular-slickgrid/extensions/headerMenuExtension.ts
+++ src/app/modules/angular-slickgrid/extensions/headerMenuExtension.ts
@@ -221,15 +221,6 @@ export class HeaderMenuExtension implements Extension {
     if (this.sharedService.grid && this.sharedService.grid.getColumns && this.sharedService.grid.setColumns && this.sharedService.grid.getColumnIndex) {
       const columnIndex = this.sharedService.grid.getColumnIndex(column.id);
       const currentColumns = this.sharedService.grid.getColumns() as Column[];
-
-      // if we're using frozen columns, we need to readjust pinning when the new hidden column is on the left pinning container
-      // we need to do this because SlickGrid freezes by index and has no knowledge of the columns themselves
-      const frozenColumnIndex = this.sharedService.gridOptions.frozenColumn || -1;
-      if (frozenColumnIndex >= 0 && frozenColumnIndex >= columnIndex) {
-        this.sharedService.grid.setOptions({ frozenColumn: frozenColumnIndex - 1 });
-      }
-
-      // then proceed with hiding the column in SlickGrid & trigger an event when done
       const visibleColumns = arrayRemoveItemByIndex(currentColumns, columnIndex);
       this.sharedService.visibleColumns = visibleColumns;
       this.sharedService.grid.setColumns(visibleColumns);
