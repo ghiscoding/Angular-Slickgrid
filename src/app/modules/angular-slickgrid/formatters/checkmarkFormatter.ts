099a85a5bd54c703e92f396c5f777ffdb34b1473
ghiscoding <gbeaulac@gmail.com>
2021-02-12 17:20:48 -0500

    refactor: fix variable call instead of method

--- src/app/modules/angular-slickgrid/formatters/checkmarkFormatter.ts
+++ src/app/modules/angular-slickgrid/formatters/checkmarkFormatter.ts
@@ -3,12 +3,13 @@ import { isNumber } from '../services/utilities';
 
 export const checkmarkFormatter: Formatter = (row: number, cell: number, value: any) => {
   let isChecked = false;
+  const isValidNumber = isNumber(value);
 
-  if (isNumber(value)) {
+  if (isValidNumber) {
     value = +value; // convert to number before doing next condition
   }
 
-  if (value === true || (isNumber && +value > 0) || (typeof value === 'string' && value.length > 0 && value.toLowerCase() !== 'false' && value !== '0')) {
+  if (value === true || (isValidNumber && +value > 0) || (typeof value === 'string' && value.length > 0 && value.toLowerCase() !== 'false' && value !== '0')) {
     isChecked = true;
   }
 
