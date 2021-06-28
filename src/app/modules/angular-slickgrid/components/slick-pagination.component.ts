4272b18301beb7d62a527f2393d65f32351bdf83
ghiscoding <gbeaulac@gmail.com>
2021-06-28 13:43:42 -0400

    fix(pagination): should be able to toggle Pagination - we should be able to toggle Pagination and it should still work as expected after showing it again - the previous code was disposing of the Pagination Service when hiding (disposing) the Pagination Component but that shouldn't be the case, disposing of the Service should strictly be handled by the Angular-Slickgrid-Component

--- src/app/modules/angular-slickgrid/components/slick-pagination.component.ts
+++ src/app/modules/angular-slickgrid/components/slick-pagination.component.ts
@@ -131,8 +131,6 @@ export class SlickPaginationComponent implements OnDestroy, OnInit {
   }
 
   dispose() {
-    this.paginationService?.dispose();
-
     // also unsubscribe all Angular Subscriptions
     this.subscriptions = unsubscribeAllObservables(this.subscriptions);
   }
