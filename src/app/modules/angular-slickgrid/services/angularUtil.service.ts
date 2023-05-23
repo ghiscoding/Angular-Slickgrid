b0d07855b6711accfc8a513a41e5aa2e9cd7e93c
Ghislain B <gbeaulac@gmail.com>
2023-05-23 11:22:04 -0400

    chore: remove unused injections

--- src/app/modules/angular-slickgrid/services/angularUtil.service.ts
+++ src/app/modules/angular-slickgrid/services/angularUtil.service.ts
@@ -1,18 +1,12 @@
-import { ApplicationRef, EmbeddedViewRef, Injectable, Injector, ViewContainerRef } from '@angular/core';
+import { EmbeddedViewRef, Injectable, ViewContainerRef } from '@angular/core';
 
 import { AngularComponentOutput } from '../models/angularComponentOutput.interface';
 
 @Injectable()
 export class AngularUtilService {
 
-  constructor(
-    private appRef: ApplicationRef,
-    private injector: Injector,
-    private vcr: ViewContainerRef,
-  ) {}
+  constructor(private vcr: ViewContainerRef) { }
 
-
-  // ref https://hackernoon.com/angular-pro-tip-how-to-dynamically-create-components-in-body-ba200cc289e6
   createAngularComponent(component: any): AngularComponentOutput {
     // Create a component reference from the component
     const componentRef = this.vcr.createComponent(component);
@@ -27,12 +21,11 @@ export class AngularUtilService {
     return { componentRef, domElement: domElem as HTMLElement };
   }
 
-  // ref https://hackernoon.com/angular-pro-tip-how-to-dynamically-create-components-in-body-ba200cc289e6
   createAngularComponentAppendToDom(component: any, targetElement?: HTMLElement | Element, clearTargetContent = false): AngularComponentOutput {
     const componentOutput = this.createAngularComponent(component);
 
     // Append DOM element to the HTML element specified
-    if (targetElement && targetElement.appendChild) {
+    if (targetElement?.appendChild) {
       if (clearTargetContent && targetElement.innerHTML) {
         targetElement.innerHTML = '';
       }
