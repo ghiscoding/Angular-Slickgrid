9436b682ccea391df8466585642a3250c219174d
ghiscoding <Ghislain.Beaulac@se.com>
2023-09-25 19:48:32 -0400

    fix: avoid setTimeout after creating dynamic component, fixes #1273 - because we used `setTimeout` in Example 22, it was creating the dynamic Component and rendering it at the bottom of the DOM, in our case just after the grid at the bottom of it. However we shouldn't need a `setTimeout` if we use `cd.detectChanges()` BUT we need to move this code into the util to create the component because we need to detect changes right after changing the instance data BUT before the getting or reading the new Component HTML... so this way we can drop the `setTimeout` and we just use  `cd.detectChanges()` and this should improve performance by 2x because we no longer need to wait a cycle for every single cell which is a huge perf when you think about it :rocket:

--- src/app/modules/angular-slickgrid/services/angularUtil.service.ts
+++ src/app/modules/angular-slickgrid/services/angularUtil.service.ts
@@ -1,4 +1,4 @@
-import { EmbeddedViewRef, Injectable, ViewContainerRef } from '@angular/core';
+import { EmbeddedViewRef, Injectable, Type, ViewContainerRef } from '@angular/core';
 
 import type { AngularComponentOutput } from '../models/angularComponentOutput.interface';
 
@@ -7,15 +7,33 @@ export class AngularUtilService {
 
   constructor(private vcr: ViewContainerRef) { }
 
-  createAngularComponent(component: any): AngularComponentOutput {
+  createAngularComponent<C>(component: Type<C>, target?: HTMLElement, data?: any): AngularComponentOutput {
     // Create a component reference from the component
     const componentRef = this.vcr.createComponent(component);
 
+    // user could provide data to assign to the component instance
+
+    if (data) {
+      Object.assign(componentRef.instance as any, data);
+
+      // NOTE: detectChanges() MUST be doene BEFORE returning the DOM element in the next step,
+      // because if we do it only after returning the rootNodes (domElement) then it won't have the instance data yet
+      // and we would have to wait an extra cycle to see the result, this basically helps with Example22
+      componentRef.changeDetectorRef.detectChanges();
+    }
+
     // Get DOM element from component
-    let domElem;
+    let domElem: HTMLElement | null = null;
     const viewRef = (componentRef.hostView as EmbeddedViewRef<any>);
+
+    // get DOM element from the new dynamic Component, make sure this is read after any data and detectChanges()
     if (viewRef && Array.isArray(viewRef.rootNodes) && viewRef.rootNodes[0]) {
       domElem = viewRef.rootNodes[0] as HTMLElement;
+
+      // when user provides the DOM element target, we will read the new Component html and use it to replace the target html
+      if (target && domElem) {
+        target.innerHTML = domElem.innerHTML;
+      }
     }
 
     return { componentRef, domElement: domElem as HTMLElement };
