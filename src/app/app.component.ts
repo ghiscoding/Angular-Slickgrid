860394de31c084c43964653342c5d28ae7b8fdcc
ghiscoding <gbeaulac@gmail.com>
2024-05-08 23:01:06 -0400

    chore: scroll to active link route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { debounceTime, filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular-SlickGrid';
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    // scroll to active link route
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .pipe(debounceTime(0))
+      .subscribe(() => {
+        const linkElm = document.querySelector('.nav-link.active');
+        if (linkElm) {
+          linkElm.scrollIntoView({ block: 'nearest' });
+        }
+      })
+  }
 }
