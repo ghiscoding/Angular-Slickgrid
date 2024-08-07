2d18c2a61c37073e98ac2180efd42550f92d3e0f
ghiscoding <gbeaulac@gmail.com>
2024-08-07 09:22:58 -0400

    chore: cleanup eslint new flat config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,6 @@
 import angular from 'angular-eslint';
 import eslint from '@eslint/js';
 import cypress from 'eslint-plugin-cypress/flat';
-import globals from 'globals';
 import n from 'eslint-plugin-n';
 import tseslint from 'typescript-eslint';
 
@@ -13,13 +12,10 @@ export default tseslint.config(
   },
   {
     extends: [
-      // Apply the recommended core rules
+      // Apply the recommended rules
       eslint.configs.recommended,
-      // Apply the recommended TypeScript rules
       ...tseslint.configs.recommended,
-      // Optionally apply stylistic rules from typescript-eslint that improve code consistency
       ...tseslint.configs.stylistic,
-      // Apply the recommended Angular rules
       ...angular.configs.tsRecommended,
     ],
     plugins: {
@@ -32,24 +28,9 @@ export default tseslint.config(
     // Set the custom processor which will allow us to have our inline Component templates extracted
     // and treated as if they are HTML files (and therefore have the .html config below applied to them)
     processor: angular.processInlineTemplates,
-    // Override specific rules for TypeScript files (these will take priority over the extended configs above)
     rules: {
-      '@angular-eslint/directive-selector': [
-        'error',
-        {
-          type: 'attribute',
-          prefix: 'app',
-          style: 'camelCase',
-        },
-      ],
-      '@angular-eslint/component-selector': [
-        'error',
-        {
-          type: 'element',
-          style: 'kebab-case',
-        },
-      ],
-
+      '@angular-eslint/directive-selector': ['error', { type: 'attribute', prefix: 'app', style: 'camelCase' }],
+      '@angular-eslint/component-selector': ['error', { type: 'element', style: 'kebab-case' }],
       '@typescript-eslint/array-type': 'off',
       '@typescript-eslint/ban-ts-comment': 'off',
       '@typescript-eslint/no-empty-function': 'off',
@@ -64,9 +45,8 @@ export default tseslint.config(
     // and inline templates as long as we have the `processor` set on our TypeScript config above)
     files: ['**/*.html'],
     extends: [
-      // Apply the recommended Angular template rules
+      // Apply the recommended Angular template rules and focus on accessibility of our apps
       ...angular.configs.templateRecommended,
-      // Apply the Angular template rules which focus on accessibility of our apps
       ...angular.configs.templateAccessibility,
     ],
     rules: {
