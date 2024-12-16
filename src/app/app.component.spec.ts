79802cbd7f24406413642997a12abbda422bcef8
ghiscoding <gbeaulac@gmail.com>
2024-12-16 16:54:48 -0500

    chore: remove unused unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'Angular-SlickGrid'`, async () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Angular-SlickGrid');
-  });
 });
