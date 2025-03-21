639d65b6631fb80d1f728304b43099cca4d4b962
ghiscoding <gbeaulac@gmail.com>
2025-03-21 00:02:12 -0400

    chore: rollback changes to hopefully pass Cypress tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { debounceTime, filter } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   constructor(private router: Router) {}
